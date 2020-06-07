c6304470b0017a3e6899b1f02ddade6050d79d93
Grigory Rodimov <grishanyar@gmail.com>
2020-06-07 14:52:39 +0300

    complete task1

---
+++ les8_hw/task1.py
@@ -0,0 +1,47 @@
+# Lesson 8 HomeWork - Task 1
+
+"""
+Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
+В рамках класса реализовать два метода.
+Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
+Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
+Проверить работу полученной структуры на реальных данных.
+"""
+
+
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        date = []
+
+        for i in day_month_year.split('-'):
+            date.append(i)
+
+        return int(date[0]), int(date[1]), int(date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 0 <= year <= 2020:
+                    return f'Текущая дата: {day, month, year}'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата: {Data.extract(self.day_month_year)}'
+
+
+today = Data('06 - 06 - 2020')
+print(today)
+print(Data.valid(8, 10, 2015))
+print(today.valid(8, 13, 2015))
+print(today.extract('11 - 11 - 2020'))
